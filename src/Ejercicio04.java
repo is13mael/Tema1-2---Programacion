a2648eef8ce724aa3a3b037f82b4ed4102087bf2
is13mael <ismaelparramoussaif@gmail.com>
2025-09-25 19:32:18 +0200

    Added 04

---
+++ src/Ejercicio04.java
@@ -0,0 +1,12 @@
+public class Ejercicio04 {
+    public static void main(String [] args){
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n","LUNES", "MARTES", "MIÉRC.", "JUEVES.", "VIERNES");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "======", "======", "======", "======", "======");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n","SI", "PROG", "SI", "ITE I", "BD");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n","SI", "PROG", "SI", "ITE I", "BD");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "LM", "PROG", "ITE I", "PROG", "SI");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "LM", "BD", "DAS", "PROG", "ED");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "BD", "BD", "PROG", "PROG", "ED");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "BD", "SASP", "PROG", "LM", "ED");
+    }
+}
