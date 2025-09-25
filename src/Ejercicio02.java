e84eb2923c8597b3af09f10a5423159675329224
is13mael <ismaelparramoussaif@gmail.com>
2025-09-25 19:26:06 +0200

    Added 02

---
+++ src/Ejercicio02.java
@@ -0,0 +1,7 @@
+public class Ejercicio02 {
+    public static void main(String[] args){
+        System.out.println("Ismael Parra Moussaif");
+        System.out.println("Calle Pedrito de los Palotes, 24 - Villanueva del Rosario (Málaga) - España");
+        System.out.println("644 27 34 38");
+    }
+}
